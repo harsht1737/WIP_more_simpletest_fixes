b5485c3103e49d0e73fef26dcbe0647d675dafa2
Harsh Tiwari <harsht@vmware.com>
2023-03-11 14:08:38 +0000

    Fix flag propagation in SingleRequestProcessingJob

--- client/client_pool/src/concord_client_pool.cpp
+++ client/client_pool/src/concord_client_pool.cpp
@@ -483,6 +483,12 @@ void SingleRequestProcessingJob::execute() {
       read_config_.request.max_reply_size = max_reply_size_;
     }
     read_config_.request.reconfiguration = flags_ & RECONFIG_FLAG_REQ;
+
+    if ((flags_ & PRIMARY_ONLY_REQ) == PRIMARY_ONLY_REQ) {
+      read_config_.request.primary_only = true;
+      // LOG_INFO(logger_, "harsht set ReadConfig primary_only flag to true in SingleRequestProcessingJob");
+    }
+
     res = processing_client_->SendRequest(read_config_, std::move(request_));
   } else {
     write_config_.request.timeout = timeout_ms_;
